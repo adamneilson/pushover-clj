739af0dd1ab579743a20750f4479419c4653bf77
Adam Neilson <adam@junkey.com>
2013-04-13 22:31:56 +0100

    Improved error handling and tests

--- src/pushover_clj/core.clj
+++ src/pushover_clj/core.clj
@@ -5,47 +5,61 @@
 
 (defn send-message [ token user message & [title url-title url device]]  
   
-  ;; Send form params as a urlencoded body (POST or PUT)
+  (try
      (let [result (http/post "https://api.pushover.net/1/messages.json" 
           {
              ;:debug true
              ;:debug-body true
              :content-type "application/x-www-form-urlencoded"
              :form-params {
-                           :token token 
-                           :user user 
-                           :title title
-                           :message message
-                           :device device
-                           :url_title url-title
-                           :url url
-                           }
+             :token token 
+             :user user 
+             :title title
+             :message message
+             :device device
+             :url_title url-title
+             :url url
+             :throw-entire-message? true
+             }
             }
           )
         ]
-        (:status result) 
+        (:status result)
       )
+    (catch Exception e 
+      ;send the status 
+      (:status (:object (.getData e))) 
+    )
+  )
   )
 
-(defn send-urgent-message [token user message]
+(defn send-urgent-message [token user message & [title url-title url device]]
   
-  ;; Send form params as a urlencoded body (POST or PUT)
+  (try
     (let [result (http/post "https://api.pushover.net/1/messages.json" {
-             ;:debug true
-             ;:debug-body true
-             :content-type "application/x-www-form-urlencoded"
-             :form-params {
-                          :token token 
-                          :user user 
-                          :message message
-                          :priority 2
-                          :expire 3600
-                          :retry 60
-                          }
+                  ;:debug true
+                  ;:debug-body true
+                  :content-type "application/x-www-form-urlencoded"
+                  :form-params {
+                  :token token 
+                  :user user 
+                  :title title
+                  :message message
+                  :priority 2
+                  :expire 3600
+                  :retry 60
+                  :url_title url-title
+                  :url url
+                  :throw-entire-message? true
+                  }
              }
              )
         ]
         (:status result) 
       )
+    (catch Exception e 
+      ;send the status 
+      (:status (:object (.getData e))) 
+    )
   )
-
+  )
--- test/pushover_clj/core_test.clj
+++ test/pushover_clj/core_test.clj
@@ -2,14 +2,26 @@
   (:use clojure.test
         pushover-clj.core))
 
+(def token "xxxxxtokenxxxxx")
+(def user "xxxxxuserxxxxxx")
+
 (deftest basic-test
   (testing "Sending a basic push."
-    (is (= 200 (send-message "xxxxxtokenxxxxx" "xxxxxuserxxxxxx" "This is a test msg")))
-    (is (= 200 (send-message "xxxxxtokenxxxxx" "xxxxxuserxxxxxx" "This is a titled test msg" "My Title")))
-    (is (= 200 (send-message "xxxxxtokenxxxxx" "xxxxxuserxxxxxx" "This is a titled test msg with a URL" "My Title" "Click here" "http://www.junkey.com")))
+    (is (= 200 (send-message token user "This is a test msg")))
+    (is (= 200 (send-message token user "This is a titled test msg" "My Title")))
+    (is (= 200 (send-message token user "This is a titled test msg with a URL" "My Title" "Click here" "http://www.junkey.com")))
     ))
 
 (deftest urgent-test
   (testing "Sending a basic push."
-    (is (= 200 (send-urgent-message "xxxxxtokenxxxxx" "xxxxxuserxxxxxx" "This is an URGENT message test ")))
+    (is (= 200 (send-urgent-message token user "This is an URGENT message test")))
+    (is (= 200 (send-urgent-message token user "This is an URGENT message test" "My URGENT Title")))
+    (is (= 200 (send-urgent-message token user "This is an URGENT message test" "My URGENT Title" "Click here" "http://www.junkey.com")))
+  ))
+
+(deftest error-test
+  (testing "Sending bad payloads."
+    (is (= 400 (send-message "" user "This is a test bad req")))
+    (is (= 400 (send-message token "" "This is a test bad req")))
+    (is (= 400 (send-message "\u9898" "#737373" "This is a test bad req")))
   ))
