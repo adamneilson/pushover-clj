86d3273966e29218f939ba5246455f69d38445c8
Adam Neilson <adam@junkey.com>
2013-04-13 14:14:22 +0100

    first cut

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject pushover-clj "0.1.0-SNAPSHOT"
-  :description "A simple wrapper for sending messages to the Pushover API"
+  :description "A simple wrapper for sending messages to the Pushover API (https://pushover.net/api)"
   :url "https://github.com/adamneilson/pushover-clj"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                  [clj-http "0.6.4"]])
+                 [org.clojure/data.json "0.2.1"]
+                  [clj-http "0.7.0"]])
